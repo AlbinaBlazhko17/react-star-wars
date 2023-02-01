35aca0ab315cb9b4597b188ee4c01735f672665a
Albinator <albinamivina@gmail.com>
2023-02-01 11:23:53 +0200

    add buttons prev and next

--- src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
+++ src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import styles from './PeopleNavigation.module.css';
 
 const PeopleNavigation = ({
@@ -7,8 +8,25 @@ const PeopleNavigation = ({
     nextPage,
     counterPage
 }) => {
+    const handleChangeNext = () => getResource(nextPage);
+    const handleChangePrev = () => getResource(prevPage);
+
     return (
-        <>Hello world</>
+        <div>
+            <Link to={`/people/?page=${counterPage - 1}`} className={styles.link}>
+                <button 
+                    onClick={handleChangePrev} 
+                    className={styles.buttons}
+                    disabled={!prevPage}
+                    >Previous</button>
+            </Link>
+            <Link to={`/people/?page=${counterPage + 1}`} className={styles.link}>
+                <button
+                     onClick={handleChangeNext}
+                     className={styles.buttons}
+                     disabled={!nextPage}>Next</button>
+            </Link>
+        </div>
     );
 }
 
