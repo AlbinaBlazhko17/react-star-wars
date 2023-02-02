82e6a2028036ee98d102eb20b933ece14e6ea3a5
Albinator <albinamivina@gmail.com>
2023-02-02 11:13:56 +0200

    add function to request data about person

--- src/components/PersonPage/PersonPage.jsx
+++ src/components/PersonPage/PersonPage.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { getApiResource } from '@utils/network';
+import { API_PERSON } from '@constants/api';
+
 import styles from './PersonPage.module.css';
 
-const PersonPage = () => {
+const PersonPage = ({ match}) => {
+    useEffect(() => {
+        (async () => {
+            const id = match.params.id;
+            const res = await getApiResource(`${API_PERSON}/${id}/`);
+        })();
+    }, [])
     return (
         <></>
     );
