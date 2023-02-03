8b1368a9b927ee30bf96521fd081e28907a1f977
Albinator <albinamivina@gmail.com>
2023-02-03 10:51:04 +0200

    show films and add func to sort

--- src/components/PersonPage/PersonFilms/PersonFilms.jsx
+++ src/components/PersonPage/PersonFilms/PersonFilms.jsx
@@ -1,10 +1,36 @@
 import { PropTypes } from 'prop-types';
+import { useEffect, useState } from 'react';
+import { makeConCurrentRequest } from '@utils/network'
+
 import styles from './PersonFilms.module.css';
 
+
 const PersonFilms = ({ personFilms }) => {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await makeConCurrentRequest(personFilms);
+
+            setFilms(response);
+        })();
+    }, [])
+
     console.log(personFilms);
     return (
-        <></>
+        <div>
+            <ul>
+                {films && films
+                            .sort((a, b) => a.episode_id - b.episode_id)
+                            .map(({title, episode_id}) => 
+                            <li key={episode_id}>
+                                <span>Episode {episode_id}</span>
+                                <span> : </span>
+                                <span>{title}</span>
+                            </li>
+                )}
+            </ul>
+        </div>
     );
 }
 
