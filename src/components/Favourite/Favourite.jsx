4b0d80472bff14ad9e5f423d7bb1acdd9120a25e
Albinator <albinamivina@gmail.com>
2023-02-07 17:45:20 +0200

    add icon favourite

--- src/components/Favourite/Favourite.jsx
+++ src/components/Favourite/Favourite.jsx
@@ -1,8 +1,12 @@
+import icon from './img/bookmark.svg';
+
 import styles from './Favourite.module.css';
 
 const Favourite = () => {
     return (
-        <></>
+        <div>
+            <img className={styles.icon} src={icon} alt="Favorites" />
+        </div>
     );
 }
 
