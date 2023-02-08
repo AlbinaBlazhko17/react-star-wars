98cfec1aaf0e68bd774287f17907d76a2df1ae7d
Albinator <albinamivina@gmail.com>
2023-02-08 13:03:42 +0200

    add functions to get, set data from local storage

--- src/store/reducers/favouriteReducer.js
+++ src/store/reducers/favouriteReducer.js
@@ -1,7 +1,8 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { addPersonToFavourite, removePersonFromFavourite } from '../actions';
+import { getLocalStorage } from '@utils/localStorage';
 
-const favourite = createReducer([],
+const favourite = createReducer(getLocalStorage('store'),
         (builder) => {
            builder
                 .addCase(addPersonToFavourite, (state, action) => { state.push(action.payload) }) 
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
+import { setLocalStorage } from '@utils/localStorage';
 
 const store = configureStore({ reducer: rootReducer});
+
+store.subscribe(() => {
+    setLocalStorage('store', store.getState().favourite);
+});
 
 export default store;
