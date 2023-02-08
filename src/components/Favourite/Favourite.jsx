bfa16f41e2cad454252f8bcf6e07d490cce31023
Albinator <albinamivina@gmail.com>
2023-02-08 12:23:27 +0200

    add counter to favourite

--- src/components/Favourite/Favourite.jsx
+++ src/components/Favourite/Favourite.jsx
@@ -1,13 +1,22 @@
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import icon from './img/bookmark.svg';
 
 import styles from './Favourite.module.css';
 
 const Favourite = () => {
+    const [count, setCount] = useState(null);
+    const storeData = useSelector(state => state.favourite);
+
+    useEffect(() => {
+        setCount(storeData.length);
+    });
+
     return (
         <div className={styles.container}>
             <Link to="/favourites">
-                <span className={styles.counter}>0</span>
+                <span className={styles.counter}>{count}</span>
                 <img className={styles.icon} src={icon} alt="Favorites" />
             </Link>
             
