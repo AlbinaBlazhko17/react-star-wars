14c751c330b5d453cb66db52be4dc0f477df9de9
Albinator <albinamivina@gmail.com>
2023-02-13 12:47:22 +0200

    get and set data from search

--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -3,17 +3,30 @@ import { PropTypes } from 'prop-types';
 import { getApiResource } from '@utils/network';
 import { API_SEARCH } from '@constants/api';
 import { withErrorApi } from '@hoc-helpers/withErrorApi';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
 
 import styles from './SearchPage.module.css';
 
 
 const SearchPage = ({ setErrorApi }) => {
     const [inputSearchValue, setInputSearchValue] = useState('');
+    const [people, setPeople] = useState([]);
 
     const getResponse = async param => {
-        const res = await getApiResource();
-        
+        const res = await getApiResource(API_SEARCH + param);
+
         if(res) {
+            const peopleList = res.results.map(({ name, url }) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+
+                return {
+                    id,
+                    name,
+                    img
+                }
+            })
+            setPeople(peopleList);
             setErrorApi(false);
         } else {
             setErrorApi(true);
