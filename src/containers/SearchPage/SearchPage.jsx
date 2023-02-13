0039640773053d4fb0ca9e7c5a20282f78ba7c2d
Albinator <albinamivina@gmail.com>
2023-02-13 12:34:39 +0200

    add function to get  search result from api

--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
 import { PropTypes } from 'prop-types';
+import { getApiResource } from '@utils/network';
+import { API_SEARCH } from '@constants/api';
+import { withErrorApi } from '@hoc-helpers/withErrorApi';
+
 import styles from './SearchPage.module.css';
 
-const SearchPage = () => {
+
+const SearchPage = ({ setErrorApi }) => {
+    const [inputSearchValue, setInputSearchValue] = useState('');
+
+    const getResponse = async param => {
+        const res = await getApiResource();
+        
+        if(res) {
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
+    }
+
+    const handleInputChange = e => {
+        const value = e.target.value;
+        setInputSearchValue(value);
+        getResponse(value)
+    }
+
     return (
-        <></>
+        <>
+            <h1 className='header__text'>Search</h1>
+            <input
+             type="text"
+             value={inputSearchValue} 
+             onChange={handleInputChange}
+             placeholder='Input character`s name '/>
+        </>
     );
 }
 
-// SearchPage.propTypes = {
-//     people: PropTypes.array
-// }
+SearchPage.propTypes = {
+    setErrorApi: PropTypes.func
+}
 
-export default SearchPage;
+export default withErrorApi(SearchPage);
